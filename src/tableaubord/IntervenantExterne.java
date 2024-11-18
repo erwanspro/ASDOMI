932dc2ace056516f12a464158fcd45eee7da8ea4
cbgam <cbgam@Overlord>
2024-11-18 17:13:57 +0100

    intervenants externe

--- src/tableaubord/IntervenantExterne.java
+++ src/tableaubord/IntervenantExterne.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tableaubord;
 
 public class IntervenantExterne extends Intervenant{
+    private String organisme;
     
-    public IntervenantExterne(String nom, String prenom, String email) {
+    public IntervenantExterne(String nom, String prenom, String email, String organisme) {
         super(nom, prenom, email);
+        this.organisme = organisme;
+    }
+
+    public String getOrganisme() {
+        return organisme;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Organisme: " + organisme;
+    }
+
+    @Override
+    public boolean isExterne() {
+        return true;
     }
     
 }
