a46504812a6020f896069b0804e50e55dcd3001f
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-18 17:09:41 +0100

    add IntervenantExt

---
+++ src/tableaubord/IntervenantExterne.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tableaubord;
+
+public class IntervenantExterne extends Intervenant{
+    
+    public IntervenantExterne(String nom, String prenom, String email) {
+        super(nom, prenom, email);
+    }
+    
+}
