9cc98fb6c05cc2297a320204209c7c5bce7e3011
cbgam <cbgam@Overlord>
2024-11-18 17:01:30 +0100

    q1 a5

--- src/tableaubord/Intervenant.java
+++ src/tableaubord/Intervenant.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tableaubord;
 
-/**
- *
- * @author esellin
- */
 public class Intervenant {
+    private String nom;
+    private String prenom;
+    private String email;
+
+    // Constructeur
+    public Intervenant(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    // Getters
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String afficherInfo() {
+        return "Nom: " + nom + ", Prénom: " + prenom + ", Email: " + email;
+    }
     
+    public boolean isExterne() {
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return "Intervenant [Nom: " + nom + ", Prénom: " + prenom + ", Email: " + email + "]";
+    }
 }
--- src/tableaubord/Principal.java
+++ src/tableaubord/Principal.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tableaubord;
-
 
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        Intervenant intervenant1 = new Intervenant("Dupont", "Jean", "jean.dupont@example.com");
+        Intervenant intervenant2 = new Intervenant("Martin", "Sophie", "sophie.martin@example.com");
 
+        System.out.println(intervenant1);
+        System.out.println(intervenant2);
+        System.out.println(intervenant1.isExterne());
+        System.out.println(intervenant2.isExterne());
     }
     
 }
