a0a2166f4488cf2356128a15752251f997ff99a2
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-18 16:41:00 +0100

    add project

--- src/tableaubord/Principal.java
+++ src/tableaubord/Principal.java
@@ -4,17 +4,14 @@
  */
 package tableaubord;
 
-/**
- *
- * @author esellin
- */
+
 public class Principal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
